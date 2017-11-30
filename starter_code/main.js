5f4483981c1f902650794c7baafbb7fc91574321
IanWFerguson <iwferguso25@gmail.com>
2017-11-29 21:12:28 -0700

    finaly

---
+++ starter_code/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+	$("#menu").click(function(){
+	//show nav
+	var navClass = $("nav").attr('class');
+	if(navClass == "show"){
+		$("nav").removeClass('show');
+	} else {
+		$("nav").addClass('show')
+	}
+
+	});
+
+});
+
+//change menu display after 
+//certain document width
+
+
+//when hamburger menu is clicked
+//show dropdown menu
+
+//
+
